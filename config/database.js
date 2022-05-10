cd9df06442d7e148cd8141d7ddfdf15f2798f78a
代长新 <daichangxin@gmail.com>
2022-05-10 16:05:28 +0800

    feat: add mysql support

--- config/database.js
+++ config/database.js
@@ -1,10 +1,19 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: env("DATABASE_CLIENT", "sqlite"),
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
+      host: env("DATABASE_HOST", "0.0.0.0"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "123456"),
     },
     useNullAsDefault: true,
   },
